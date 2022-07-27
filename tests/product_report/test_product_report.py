ca603af666dbc13350d5f1088ab9af285936a2e2
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-07-27 17:49:11 -0300

    Teste: Testa o relatório do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        11,
+        "Minoxidil",
+        "KIRKLAND SIGNATURE",
+        "2022-02-01",
+        "2023-02-01",
+        "1CK1123",
+        "instrucao 11",
+    )
+
+    assert product.__repr__() == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
