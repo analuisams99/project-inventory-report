ff2c6eff06c43c690d98a7c027463f27ae5b12a1
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-07-27 17:42:46 -0300

    Teste: Testa a criação do produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        11,
+        "Minoxidil",
+        "KIRKLAND SIGNATURE",
+        "2022-02-01",
+        "2023-02-01",
+        "1CK1123",
+        "instrucao 11",
+    )
+
+    assert product.id == 11
+    assert product.nome_do_produto == "Minoxidil"
+    assert product.nome_da_empresa == "KIRKLAND SIGNATURE"
+    assert product.data_de_fabricacao == "2022-02-01"
+    assert product.data_de_validade == "2023-02-01"
+    assert product.numero_de_serie == "1CK1123"
+    assert product.instrucoes_de_armazenamento == "instrucao 11"
