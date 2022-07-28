0f216522d5025b8b2f0d15686ffc2a5186c3056d
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-07-28 14:48:09 -0300

    Testes: Testa a geração de uma versão do relatório em cores

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,16 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+from .mocks import list_mock, expected_return_simple, expected_return_complete
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    assert (
+        ColoredReport(SimpleReport).generate(list_mock)
+        == expected_return_simple
+    )
+    assert (
+        ColoredReport(CompleteReport).generate(list_mock)
+        == expected_return_complete
+    )
