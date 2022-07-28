990a4a8fa435563286d7c03236074bb86ea414ca
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-07-28 15:46:26 -0300

    fix: Arruma erro de lint do projeto

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -2,7 +2,45 @@ from inventory_report.reports.colored_report import ColoredReport
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
-from .mocks import list_mock, expected_return_simple, expected_return_complete
+
+list_mock = [
+    {
+        "data_de_fabricacao": "2022-07-02",
+        "data_de_validade": "2022-08-02",
+        "nome_da_empresa": "Bombril",
+    },
+    {
+        "data_de_fabricacao": "2022-07-03",
+        "data_de_validade": "2022-07-31",
+        "nome_da_empresa": "Bombril",
+    },
+    {
+        "data_de_fabricacao": "2022-07-04",
+        "data_de_validade": "2022-07-12",
+        "nome_da_empresa": "Bombril",
+    },
+    {
+        "data_de_fabricacao": "2022-07-05",
+        "data_de_validade": "2022-07-14",
+        "nome_da_empresa": "Assolan",
+    },
+]
+
+expected_return_simple = (
+    "\033[32mData de fabricação mais antiga:\033[0m "
+    "\033[36m2022-07-02\033[0m\n"
+    "\033[32mData de validade mais próxima:\033[0m "
+    "\033[36m2022-07-12\033[0m\n"
+    "\033[32mEmpresa com mais produtos:\033[0m "
+    "\033[31mBombril\033[0m"
+)
+
+expected_return_complete = (
+    f"{expected_return_simple}\n"
+    f"Produtos estocados por empresa:\n"
+    f"- Bombril: 3\n"
+    f"- Assolan: 1\n"
+)
 
 
 def test_decorar_relatorio():
